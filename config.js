2a5b4c359dcbb446c9cf7a59b3b04f143fc68c2e
msaidguven <msadguven@gmail.com>
2026-06-15 20:21:30 +0300

    ver_1.0.1.18

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = 'https://hsdrpjgswsahtnmwobll.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_HMNycDbCD-n3kdoJAk_nxw_00IWbKWb';
+const SUPABASE_URL = 'https://pwzbjhgrhkcdyowknmhe.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_cXSIkRvdM3hsu2ZIFjSYVQ_XRhlmng8';
     
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
