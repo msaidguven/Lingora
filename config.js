1a2e2e797435e6b89edd56802f28795d207309c7
msaidguven <msadguven@gmail.com>
2026-06-15 20:46:44 +0300

    ver_1.0.1.18

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = 'https://pwzbjhgrhkcdyowknmhe.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3emJqaGdyaGtjZHlvd2tubWhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyOTgwNTYsImV4cCI6MjA4MDg3NDA1Nn0.rCrirZxjXSNDO5R5NPur3ac_153Z4FIvd85jEz-uXKY';
+const SUPABASE_URL = "https://pwzbjhgrhkcdyowknmhe.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3emJqaGdyaGtjZHlvd2tubWhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyOTgwNTYsImV4cCI6MjA4MDg3NDA1Nn0.rCrirZxjXSNDO5R5NPur3ac_153Z4FIvd85jEz-uXKY";
     
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
