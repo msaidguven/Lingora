6ad79c5dcf3a8dfc675914749ef48859e7818305
msaidguven <msadguven@gmail.com>
2026-06-15 19:31:32 +0300

    ver_1.0.1.18

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "BURAYA_PROJECT_URL";
-const SUPABASE_ANON_KEY = "BURAYA_ANON_KEY";
+const SUPABASE_URL = 'https://hsdrpjgswsahtnmwobll.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_HMNycDbCD-n3kdoJAk_nxw_00IWbKWb';
+    
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
