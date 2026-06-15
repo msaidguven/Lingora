52681edbdba9e2d57febaeb3d2b59a84c0f2bcd5
msaidguven <msadguven@gmail.com>
2026-06-16 01:11:50 +0300

    ver_1.0.1.18

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Admin from "./Admin.jsx";
 
-import { supabase } from "../config.js";  
+import { supabase } from "./config.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
