b18206c93bfeb1fc9a95a185702c01b1050d153f
msaidguven <msadguven@gmail.com>
2026-07-17 23:02:55 +0300

    ver_1.0.1.18

--- src/components/Header/config/navConfig.js
+++ src/components/Header/config/navConfig.js
@@ -5,7 +5,7 @@
 
 export const NAV_ITEMS = [
   { key: "home", icon: "ti-home", label: "Anasayfa" },
-  { key: "dashboard", icon: "ti-layout-dashboard", label: "İstatistik" },
+  { key: "StatsScreen", icon: "ti-layout-dashboard", label: "İstatistik" },
   { key: "quiz", icon: "ti-tournament", label: "Kelime/Cümle" },
 ];
 
